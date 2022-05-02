841f7e9115e2eea07293dd403bf7b313b6c5b08e
BoomBakar <abubakardar608@gmail.com>
2022-05-02 15:08:03 +0500

    CMS added

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibrary18;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMS s = new CMS();
+            s.displayMenu();
+        }
+    }
+}
